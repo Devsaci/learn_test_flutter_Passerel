d28cb29c643860ae19db2ae6d34b690978ec3525
saci zakaria <sacideveloppeur@gmail.com>
2022-11-13 22:17:13 +0100

    primaryColor: Colors.deepOrange,

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       title: " Flutter Demo",
-      theme: ThemeData(),
+      theme: ThemeData(
+        primaryColor: Colors.deepOrange,
+      ),
     );
   }
 }
