3d740090bb014c6f55e4d120f1959cce4d0fb38f
saci zakaria <sacideveloppeur@gmail.com>
2022-11-13 22:00:45 +0100

    title: const Text("Apprendre les tests"),

--- lib/custom_app_bar.dart
+++ lib/custom_app_bar.dart
@@ -1,7 +1,9 @@
-
-
 import 'package:flutter/material.dart';
 
-class CustomAppBar extends AppBar{
-  CustomAppBar({Key? key}) : super(key: key);
+class CustomAppBar extends AppBar {
+  CustomAppBar({Key? key})
+      : super(
+          key: key,
+          title: const Text("Apprendre les tests"),
+        );
 }
