e5b1ccb1686efa5830bebbd0a6c10294917fb200
saci zakaria <sacideveloppeur@gmail.com>
2022-11-13 22:15:45 +0100

    theme: ThemeData(),

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-class MyApp extends StatelessWidget{
-  const MyApp({Key? key}) : super(key: key);
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-   return const MaterialApp();
+    return  MaterialApp(
+      title: " Flutter Demo",
+      theme: ThemeData(),
+    );
   }
 }
