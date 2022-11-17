9fde0242124076396258b1ab8e1c459b9c2f962d
saci zakaria <sacideveloppeur@gmail.com>
2022-11-17 10:45:18 +0100

    test('Construction d\'un Objet Item', () {});

--- test/item_test.dart
+++ test/item_test.dart
@@ -3,3 +3,6 @@ import 'package:flutter_test_passerel/item.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 
+main() {
+  test('Construction d\'un Objet Item', () {});
+}
