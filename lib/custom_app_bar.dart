76d9a173dd24a3382d1daf81cd31a3b5d474784c
saci zakaria <sacideveloppeur@gmail.com>
2022-11-17 14:16:32 +0100

    Fix bug

--- lib/custom_app_bar.dart
+++ lib/custom_app_bar.dart
@@ -3,13 +3,14 @@ import 'package:flutter/material.dart';
 class CustomAppBar extends AppBar {
   CustomAppBar({Key? key})
       : super(
-          key: key,
-          title: const Text("Apprendre les tests"),
-          leading: Image.asset('images/androiddummy.png'),
-    actions: [
-      Icon(Icons.connected_tv_sharp),
-      SizedBox(width: 10,),
-      Icon(Icons.add_alert),
-    ]
-        );
+            key: key,
+            title: const Text("Apprendre les tests"),
+            leading: Image.asset('images/androiddummy.png'),
+            actions: [
+              const Icon(Icons.connected_tv_sharp),
+              const SizedBox(
+                width: 10,
+              ),
+              const Icon(Icons.add_alert),
+            ]);
 }
--- test/item_test.dart
+++ test/item_test.dart
@@ -1,7 +1,6 @@
 //14. Créer une class de test
 import 'package:flutter_test_passerel/item.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
 
 main() {
   test('Construction d\'un Objet Item', () {
