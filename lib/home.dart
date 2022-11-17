a03f7df7d15e51392b61242b2ddd6bce611942c2
saci zakaria <sacideveloppeur@gmail.com>
2022-11-17 10:29:26 +0100

    Extract Method add()

--- lib/home.dart
+++ lib/home.dart
@@ -68,16 +68,21 @@ class _HomeState extends State<Home> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          if (controller.text != "") {
-            setState(() {
-              final item = Item(name: controller.text);
-              controller.text = "";
-            });
-          }
+          add();
         },
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  void add() {
+    if (controller.text != "") {
+      setState(() {
+        final item = Item(name: controller.text);
+        controller.text = "";
+        items.add(item);
+      });
+    }
   }
 
   dismiss(index) {
