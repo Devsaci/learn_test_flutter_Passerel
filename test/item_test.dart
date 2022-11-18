06914a2b8a888e112342372a08eb574823ebc0ba
saci zakaria <sacideveloppeur@gmail.com>
2022-11-18 09:37:03 +0100

    expect(item.isChecked, true);

--- test/item_test.dart
+++ test/item_test.dart
@@ -22,6 +22,6 @@ main() {
   test('Test du changement de check', () {
     Item item = newItem();
     item.updateCheck();
-    expect(item.updateCheck(), true);
+    expect(item.isChecked, true);
   });
 }
