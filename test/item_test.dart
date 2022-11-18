b805cce79305428be24c1ecf6f4f789a297251d6
saci zakaria <sacideveloppeur@gmail.com>
2022-11-18 09:21:53 +0100

    Item newItem()

--- test/item_test.dart
+++ test/item_test.dart
@@ -3,9 +3,18 @@ import 'package:flutter_test_passerel/item.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 main() {
+  String name = "Test d'un Item";
+  //Item item = Item(name: name);
+
+  Item newItem() {
+    //String name = "Test d'un Item";
+    return Item(name: name);
+  }
+
   test('Construction d\'un Objet Item', () {
-    String name = "Test d'un Item";
-    Item item = Item(name: name);
+    // String name = "Test d'un Item";
+    //Item item = Item(name: name);
+    Item item = newItem();
     expect(item.name, name);
     expect(item.isChecked, false);
   });
