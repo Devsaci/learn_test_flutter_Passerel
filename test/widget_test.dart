62e31e247fb7f5e76bb0a6440ad3a8ed3e490c43
saci zakaria <sacideveloppeur@gmail.com>
2022-11-20 18:25:30 +0100

    testWidgets('Test set', (WidgetTester tester)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,4 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_test_passerel/home.dart';
 
-void main() {}
+void main() {
+  testWidgets('Test set', (WidgetTester tester) async {
+    tester.pumpWidget(const Home());
+  });
+}
