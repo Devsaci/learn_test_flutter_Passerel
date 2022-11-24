6bb114384769347cb501a78f38bfe7e49422cd81
saci zakaria <sacideveloppeur@gmail.com>
2022-11-24 15:35:31 +0100

    application.main();

--- test_driver/app.dart
+++ test_driver/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_driver/driver_extension.dart';
-import 'package:flutter_test_passerel/main.dart';
+import 'package:flutter_test_passerel/main.dart' as application;
 
 void main() {
   enableFlutterDriverExtension();
+  application.main();
 }
