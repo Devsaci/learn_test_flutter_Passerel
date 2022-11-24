dc42f77d317d9a271d06d544e76679457ed57f4e
saci zakaria <sacideveloppeur@gmail.com>
2022-11-24 15:42:03 +0100

    Step by Step

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,4 +1,10 @@
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
-void main() {}
+void main() {
+  //Crée un Driver
+
+  // Mettre en place ce Driver
+
+  //Supprime à la fin du test
+}
